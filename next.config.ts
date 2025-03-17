4d5830279b46a9411ed6edb6e820f4b9f258077b
KionBreadley01 <juansanchezjuan125@gmail.com>
2025-03-16 18:14:59 -0600

    Get properties in the front end list them

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */ 
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000', // Correcto, 8000 en lugar de 80000
+        pathname: '/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
